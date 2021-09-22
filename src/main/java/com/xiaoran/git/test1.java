2d731efc0d3e137ec049d0b4e78abbab7daf1c7b
RJN <RJN@xiaoran.com>
2021-09-22 10:54:54 +0800

    test1

---
+++ src/main/java/com/xiaoran/git/test1.java
@@ -0,0 +1,12 @@
+package com.xiaoran.git;
+
+public class test1 {
+    public static void main(String args[]){
+        System.out.println("小冉");
+        System.out.println("小冉");
+        System.out.println("小冉");
+        System.out.println("小冉");
+        System.out.println("小冉");
+
+    }
+}
